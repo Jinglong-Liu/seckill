c222287236eecbf6f408d2a6477a2fa85da6f71d
ljl <1601029287@qq.com>
2022-02-28 22:09:46 +0800

    MD5

---
+++ src/main/java/cn/edu/nju/seckill/utils/MD5Util.java
@@ -0,0 +1,35 @@
+package cn.edu.nju.seckill.utils;
+
+import com.sun.jmx.snmp.internal.SnmpSubSystem;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.ibatis.logging.stdout.StdOutImpl;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.SocketHandler;
+
+@Component
+public class MD5Util {
+    public static String md5(String src){
+        return DigestUtils.md2Hex(src);
+    }
+    private static final String salt = "1a2b3c4d";
+    public static String inputPassToFromPass(String inputPass){
+        String str = salt.charAt(0) + salt.charAt(0) + inputPass + salt.charAt(5) + salt.charAt(4);
+        return md5(str);
+    }
+    public static String fromPassToDBPass(String fromPass,String salt){
+        String str = salt.charAt(0) + salt.charAt(0) + fromPass + salt.charAt(5) + salt.charAt(4);
+        return md5(str);
+    }
+    public static String inputPassToDBPass(String inputPass,String salt){
+        String fromPass = inputPassToFromPass(inputPass);
+        String dbPass = fromPassToDBPass(fromPass, salt);
+        return dbPass;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(inputPassToFromPass("123456"));//a86ceca3a64d8cea4b031f08ff91c110
+        System.out.println(fromPassToDBPass(inputPassToFromPass("123456"),"1a2b3c4d"));
+        System.out.println(inputPassToDBPass("123456","1a2b3c4d"));
+    }
+}
