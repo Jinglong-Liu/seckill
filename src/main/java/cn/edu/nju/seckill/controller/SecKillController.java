bff9260a5c439e4e1614d8cc1357307681199469
ljl <1601029287@qq.com>
2022-03-03 10:56:29 +0800

    service update

--- src/main/java/cn/edu/nju/seckill/controller/SecKillController.java
+++ src/main/java/cn/edu/nju/seckill/controller/SecKillController.java
@@ -67,8 +67,8 @@ public class SecKillController implements InitializingBean {
             return RespBean.error(RespBeanEnum.EMPTY_STOCK);
         }
         // 预减库存
-        //Long stock = valueOperations.decrement("seckillGoods:" + goodsId);
-        Long stock = (Long) redisTemplate.execute(script, Collections.singletonList("seckillGoods:" + goodsId),Collections.EMPTY_LIST);
+        Long stock = valueOperations.decrement("seckillGoods:" + goodsId);
+        //Long stock = (Long) redisTemplate.execute(script, Collections.singletonList("seckillGoods:" + goodsId),Collections.EMPTY_LIST);
         if(stock < 0){
             emptyStockMap.put(goodsId,true);
             valueOperations.increment("seckillGoods:" + goodsId);//不要变长负数
